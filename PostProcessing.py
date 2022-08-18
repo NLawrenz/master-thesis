260483dbae6c060638777d9dd6107ac22ffcc785
Nicolai <nicolai.lawrenz@gmail.com>
2022-08-18 14:30:18 +0200

    preprocessing and PostProcessing

---
+++ PostProcessing.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def abstention_fit(x_test,y_prob, low = 0, high = 0.16, step = 0.01, error = 'symmetric'):
+    if error is 'symmetric':
+        return 
+
+    else:
+        return
